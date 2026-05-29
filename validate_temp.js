b032ae58959a2e3308e07b11f4c3e16120cd66c4
designstudiocms-source <designstudiocms@gmail.com>
2026-05-30 02:00:52 +0530

    Delete validate_temp.js

--- validate_temp.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = "c:/Users/ESHA/Desktop/Codex/My Project/temp_script.js";
-try {
-  new Function(fs.readFileSync(path, 'utf8'));
-  console.log('VALID_JS');
-} catch (e) {
-  console.error('JS_ERROR', e.message);
-  process.exit(1);
-}
